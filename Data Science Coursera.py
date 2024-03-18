17de60e6c16a1bec49064f630566e70be81e659b
shivanshmani <142907422+shivanshmani@users.noreply.github.com>
2024-03-19 01:15:58 +0530

    Add files via upload

---
+++ Data Science Coursera.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook
+
+# ## Data Science Tools and Ecosystem
+
+# ## Introduction
+
+# The languages used are: 
+# 1. Python
+# 2. R
+# 3. Julia
+
+# Some commonly used libraries are: 
+# 1. Numpy
+# 2. Pandas
+# 3. Sk-learn
+
+# ### 
+# 
+# | Data Science Tools |
+# |:--------:|
+# |  Jupyter  | 
+# |  Visual Sudio Code | 
+# |  R Studio   |  
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3 * 4) + 5
+result
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# ### Objectives:
+# -  List popular languages for Data Science
+# -  List commonly used libraries in Data Science
+# -  Introduce basic arithmetic expressions in Python
+
+# ### Author's Name:
+#  
+#  Shivansh Mani Tripathi
+
+# In[ ]:
+
+
+
+
